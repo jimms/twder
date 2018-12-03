ef531495a043bfec74750d8e76fdf217ff6b7bc7
jimms.hsieh <jimms.hsieh@gmail.com>
2018-12-03 22:53:06 +0900

    Bump version

--- twder/__init__.py
+++ twder/__init__.py
@@ -1,7 +1,7 @@
 from .api import *
 
 __title__ = 'twder'
-__version__ = '0.1.3'
+__version__ = '0.1.4'
 __author__ = 'Jimms Hsieh'
 __license__ = 'MIT'
 __copyright__ = 'Copyright (C) 2017 Jimms Hsieh'
